26c387cd717d4a62632f3753348bfe887a75617c
Inliang <79506519+Inliang@users.noreply.github.com>
2026-03-30 22:03:04 +0800

    Create ouc-online.user.js

---
+++ ouc-online.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         OUC Online Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A userscript for OUC Online
+// @author       Inliang
+// @match        *://*.ouc-online.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your code here...
+})();
